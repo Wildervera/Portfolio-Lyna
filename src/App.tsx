85b8e4a59d27171f03fdf3dc468c2ee110ad6f74
Lyna Seridji <lynaseridji@Lynas-MacBook-Air.local>
2026-03-04 19:58:26 -0500

    fix: scroll to top on all route changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import { About, Portfolio } from './pages';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Portfolio />} />
-      <Route path="/project/:id" element={<Portfolio />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Portfolio isWorkPage={true} />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/project/:id" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Portfolio isWorkPage={true} />} />
+      </Routes>
+    </>
   );
 };
 
