c58845d251f3a365e603f345d24cbe55013b84c7
Lyna Seridji <lynaseridji@Lynas-MacBook-Air.local>
2026-03-04 19:48:04 -0500

    style: hide default cursor on About page by using GLOBAL_STYLES

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Header } from '../components';
 import { CustomCursor } from '../components/CustomCursor';
+import { GLOBAL_STYLES } from '../styles/global';
 
 // Experience data
 const experiences = [
@@ -61,6 +62,7 @@ export const About: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-[#FAF9F6]">
+      <style>{GLOBAL_STYLES}</style>
       <CustomCursor />
       <Header />
 
